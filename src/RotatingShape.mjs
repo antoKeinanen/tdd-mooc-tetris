6ee96a182e9d47826df6e65533786b9a2e88cd8f
Anto Keinänen <antokeinanen@iki.fi>
2026-05-07 18:18:30 +0300

    add boilerplate for rotations

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -3,20 +3,22 @@ export class RotatingShape {
     width;
     height;
     orientations;
+    rotations;
 
-    constructor(shape, width, height, orientations = 4) {
+    constructor(shape, width, height, orientations = 4, rotations = 0) {
         this.shape = shape;
         this.width = width;
         this.height = height;
         this.orientations = orientations;
+        this.rotations = rotations;
     }
 
-    static fromString(string, orientations = 4) {
+    static fromString(string, orientations = 4, rotations = 0) {
         const rows = string.replaceAll(" ", "").split("\n");
         const width = rows[0].length;
         const height = rows.length;
         const shape = rows.reduce((prev, row) => [...prev, ...row.split("")], []);
-        return new RotatingShape(shape, width, height, orientations);
+        return new RotatingShape(shape, width, height, orientations, rotations);
     }
 
     toString() {
@@ -27,13 +29,13 @@ export class RotatingShape {
         const newShape = this.shape.map((_, i) => {
             return this.shape[(this.width - 1 - (i % this.width)) * this.width + Math.floor(i / this.width)];
         });
-        return new RotatingShape(newShape, this.width, this.height, this.orientations);
+        return new RotatingShape(newShape, this.width, this.height, this.orientations, this.rotations);
     }
 
     rotateLeft() {
         const newShape = this.shape.map((_, i) => {
             return this.shape[i % this.width * this.width + (this.width - 1 - Math.floor(i / this.width))];
         });
-        return new RotatingShape(newShape, this.width, this.height, this.orientations);
+        return new RotatingShape(newShape, this.width, this.height, this.orientations, this.rotations);
     }
 }
