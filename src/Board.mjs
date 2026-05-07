38a6e7d36442d3128a644b8e9c4c2fa3e8bb1fdb
Anto Keinänen <antokeinanen@iki.fi>
2026-05-07 13:47:36 +0300

    When a block is dropped it starts from the top middle

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,13 +1,19 @@
 export class Board {
   width;
   height;
+  board;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.board = Array(width * height).fill(".")
   }
 
   toString() {
-    return (".".repeat(this.width) + "\n").repeat(this.height);
+    return this.board.reduce((p, c, i) => p  + (i % this.width === 0 && i != 0 ? "\n" : "") + c) + "\n"
+  }
+
+  drop(shape) {
+    this.board[Math.floor(this.width / 2)] = shape;
   }
 }
