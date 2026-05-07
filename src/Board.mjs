cf03f6ee25bb2f05d684544eaca311b18dcc6a52
Anto Keinänen <antokeinanen@iki.fi>
2026-05-07 13:53:01 +0300

    mark new blocks as falling

--- src/Board.mjs
+++ src/Board.mjs
@@ -2,18 +2,22 @@ export class Board {
   width;
   height;
   board;
+  moving;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
-    this.board = Array(width * height).fill(".")
+    this.board = Array(width * height).fill(".");
+    this.moving = [];
   }
 
   toString() {
-    return this.board.reduce((p, c, i) => p  + (i % this.width === 0 && i != 0 ? "\n" : "") + c) + "\n"
+    return this.board.reduce((p, c, i) => p + (i % this.width === 0 && i != 0 ? "\n" : "") + c) + "\n";
   }
 
   drop(shape) {
-    this.board[Math.floor(this.width / 2)] = shape;
+    const blockCoord = Math.floor(this.width / 2);
+    this.board[blockCoord] = shape;
+    this.moving.push(blockCoord);
   }
 }
