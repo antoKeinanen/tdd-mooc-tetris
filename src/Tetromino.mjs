559bdc1993f16f5ce5c6e5f4b69f2e6271460d14
Anto Keinänen <antokeinanen@iki.fi>
2026-05-07 19:50:04 +0300

    add O-shape

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -3,4 +3,5 @@ import { RotatingShape } from "./RotatingShape.mjs";
 export class Tetromino extends RotatingShape {
     static T_SHAPE = Tetromino.fromString(".T.\nTTT\n...");
     static I_SHAPE = Tetromino.fromString(".....\n.....\nIIII.\n.....\n.....", 2);
+    static O_SHAPE = Tetromino.fromString(".OO\n.OO\n...", 1);
 }
