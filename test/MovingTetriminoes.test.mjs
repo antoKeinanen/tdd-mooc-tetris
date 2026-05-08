7646c3a0e412af00c39c409d7b95ba55917b35a1
Anto Keinänen <antokeinanen@iki.fi>
2026-05-08 16:45:54 +0300

    add tests

---
+++ test/MovingTetriminoes.test.mjs
@@ -0,0 +1,43 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+
+describe("a falling tetromino can be moved", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+  });
+
+
+  test("left", () => {
+    board.drop(Tetromino.T_SHAPE);
+    board.left();
+
+
+    expect(board.toString()).to.equalShape(
+      `...T......
+       ..TTT.....
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+   });
+
+  test.skip("right", () => {
+    expect(shape.rotateRight().toString()).to.equalShape(
+      `GDA
+       HEB
+       IFC`
+    );
+  });
+
+  test.skip("down", () => {
+    expect(shape.rotateLeft().toString()).to.equalShape(
+      `CFI
+       BEH
+       ADG`
+    );
+  });
+});
